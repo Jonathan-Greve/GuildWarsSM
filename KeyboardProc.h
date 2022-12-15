b2f098bda9b16f32fdaf9da8736d6498c06a4f20
Jonathan-Greve <jonagrev@gmail.com>
2022-12-15 08:54:11 +0800

    Formatting

--- KeyboardProc.h
+++ KeyboardProc.h
@@ -1,13 +1,15 @@
 #pragma once
 #include "GuildWarsSM.h"
 
-LRESULT CALLBACK KeyboardProc(_In_ int code, _In_ WPARAM wParam,
-                              _In_ LPARAM lParam) {
-    if (code < 0) {
+LRESULT CALLBACK KeyboardProc(_In_ int code, _In_ WPARAM wParam, _In_ LPARAM lParam)
+{
+    if (code < 0)
+    {
         return CallNextHookEx(nullptr, code, wParam, lParam);
     }
 
-    if (wParam == VK_END) {
+    if (wParam == VK_END)
+    {
         GuildWarsSM::Instance().Terminate();
     }
 
